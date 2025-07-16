f412128de6efaf80fe1e1d6ca247ed884feddef6
Dewantara <drawww111@gmail.com>
2025-07-16 20:39:23 +0900

    Add Sppd routes for sekretaris and staff roles
    Introduced new routes for SppdController under sekretaris and staff middleware groups, enabling CRUD operations and PDF generation for SPPD. Also updated user update route to use PUT and renamed sekretaris dashboard route for consistency.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\KasubagController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\RedirectController;
 use App\Http\Controllers\SekretarisController;
+use App\Http\Controllers\SppdController;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
@@ -32,7 +33,7 @@ Route::middleware(['auth', 'checkrole:1'])->group(function () {
     Route::get('/admin/users/create', [UsersController::class, 'create'])->name('admin.users.create');
     Route::post('/admin/users/store', [UsersController::class, 'store'])->name('admin.users.store');
     Route::get('/admin/users/edit/{id}', [UsersController::class, 'edit'])->name('admin.users.edit');
-    Route::post('/admin/users/update/{id}', [UsersController::class, 'update'])->name('admin.users.update');
+    Route::put('/admin/users/update/{id}', [UsersController::class, 'update'])->name('admin.users.update');
     Route::delete('/admin/users/destroy/{id}', [UsersController::class, 'destroy'])->name('admin.users.destroy');
 
     //CURD Data Pegawai
@@ -45,8 +46,9 @@ Route::middleware(['auth', 'checkrole:1'])->group(function () {
 });
 
 Route::middleware(['auth', 'checkrole:2'])->group(function () {
-    Route::get('/sekre', [SekretarisController::class, 'index'])->name('sekre.dashboard');
+    Route::get('/sekretaris', [SekretarisController::class, 'index'])->name('sekre.dashboard');
     // Add other sekretaris routes here
+    Route::get('/sekretaris/sppd', [SppdController::class, 'index'])->name('sekretaris.sppd.index');
 });
 
 Route::middleware(['auth', 'checkrole:3'])->group(function () {
@@ -57,4 +59,13 @@ Route::middleware(['auth', 'checkrole:3'])->group(function () {
 Route::middleware(['auth', 'checkrole:4'])->group(function () {
     Route::get('/staff', [StaffController::class, 'index'])->name('staff.dashboard');
     // Add other staff routes here
+
+    Route::get('/staff/sppd', [SppdController::class, 'index'])->name("staff.sppd.index");
+    Route::get('/staff/sppd/create', [SppdController::class, 'create'])->name('staff.sppd.create');
+    Route::post('/staff/sppd/store', [SppdController::class, 'store'])->name('staff.sppd.store');
+    Route::get('/staff/sppd/show/{id}', [SppdController::class, 'show'])->name('staff.sppd.show');
+    Route::get('/staff/sppd/edit/{id}', [SppdController::class, 'edit'])->name('staff.sppd.edit');
+    Route::put('staff/sppd/update/{id}', [SppdController::class, 'update'])->name('staff.sppd.update');
+    Route::delete('staff/sppd/delete/{sppd}', [SppdController::class, 'destroy'])->name('staff.sppd.destroy');
+    Route::get('/staff/sppd/pdf/{id}', [SppdController::class, 'getSppdPdf'])->name('staff.sppd.pdf');
 });
